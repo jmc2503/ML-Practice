a300dccbe2ecbbedfb07cd086a14d1893931085a
Jimmy Connelly <j.connelly2503@gmail.com>
2024-01-04 20:28:58 -0500

    Imported initial libraries

--- mario.py
+++ mario.py
@@ -1,1 +1,7 @@
-#first comment
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+env = gym_super_mario_bros.make("SuperMarioBros-v0")
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
